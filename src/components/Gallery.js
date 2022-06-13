6038fb4e88775a87ad80fea96ca69c9c66ed3b55
Anthony Connell <toco85@gmail.com>
2022-06-13 19:43:11 -0400

    Added useRef

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,20 +1,20 @@
-import { DataContext } from '../context/DataContext';
-import GalleryItem from './GalleryItem';
-import { useContext } from 'react';
+import { useContext } from 'react'
+import { DataContext } from '../context/DataContext'
+import GalleryItem from './GalleryItem'
 
-const Gallery = () => {
+function Gallery() {
     const data = useContext(DataContext)
-
-    const display = data.map((item, index) => {
+    
+    const display = data.map((item,index) => {
         return (
-            <GalleryItem key={index} item={item} />
+            <GalleryItem item={item} key={index} />
         )
     })
-}
-
     return (
         <div>
             {display}
         </div>
     )
+}
+
 export default Gallery
